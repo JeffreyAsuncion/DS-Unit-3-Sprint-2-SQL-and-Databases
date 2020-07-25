b8cbcb8b95d4a8c3df3cc385f36adf7fcc52bbcc
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-07-25 10:29:19 -0400

    .env workds

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -1,34 +1,30 @@
 import os
-from dotenv import load_dotenv
 import psycopg2
-
-# Purge the virtualenv by pipenv --rm
-# Install binary version: pipenv install psycopg2-binary
+from dotenv import load_dotenv
 
 load_dotenv() #> loads contents of the .env file into the script's environment
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
 
-DB_NAME = os.getenv(DB_NAME)
-DB_USER = os.getenv(DB_USER)
-DB_PASS = os.getenv(DB_PASS)
-DB_HOST = os.getenv(DB_HOST)
-
+# sanity check if this really works
 print(DB_NAME, DB_USER, DB_PASS, DB_HOST)
+# exit() # to check the .env creditials were passed
 
-exit()
-
-### Connect to ElephantSQL-hosted PostgreSQL
+# Connect to ElephantSQL-hosted PostgreSQL
 conn = psycopg2.connect(dbname=DB_NAME, 
                         user=DB_USER,
                         password=DB_PASS, 
                         host=DB_HOST)
 
-### A "cursor", a structure to iterate over db records to perform queries
+# A "cursor", a structure to iterate over db records to perform queries
 cursor = conn.cursor()
 
-### An example query
-cursor.execute('SELECT * from test_table;')
+# An example query
+cursor.execute('SELECT * from test_table;') # in postgreSQL cannot fetch here
 
-### Note - nothing happened yet! We need to actually *fetch* from the cursor
+# Note - nothing happened yet! We need to actually *fetch* from the cursor
 results = cursor.fetchall()
 print(results)

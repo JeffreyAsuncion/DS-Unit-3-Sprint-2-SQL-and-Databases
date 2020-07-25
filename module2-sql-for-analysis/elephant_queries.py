0602095b8cc4324e2b6abc4c359889e38190647b
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-07-25 13:12:10 -0400

    Lecture additions from DS15

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -1,6 +1,9 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
+import json
+from psycopg2.extras import execute_values
+import pandas as pd
 
 load_dotenv() #> loads contents of the .env file into the script's environment
 
@@ -22,9 +25,87 @@ conn = psycopg2.connect(dbname=DB_NAME,
 # A "cursor", a structure to iterate over db records to perform queries
 cursor = conn.cursor()
 
-# An example query
-cursor.execute('SELECT * from test_table;') # in postgreSQL cannot fetch here
 
-# Note - nothing happened yet! We need to actually *fetch* from the cursor
+# FETCH DATA
+
+cursor.execute('SELECT * from test_table;') # in postgreSQL cannot fetch here
 results = cursor.fetchall()
-print(results)
+# print(results)
+
+
+
+################## Connect to SQLite3 db for RPG data ###################
+
+
+import sqlite3
+
+sl_conn = sqlite3.connect("rpg_db.sqlite3")
+sl_cursor = sl_conn.cursor()
+characters = sl_cursor.execute("SELECT * FROM charactercreator_character LIMIT 10;").fetchall()
+print(characters)
+
+
+################## Create Character Table in PostGRES ###################
+
+
+create_character_table_query = '''
+CREATE TABLE IF NOT EXISTS rpg_characters (
+    character_id SERIAL PRIMARY KEY,
+	name VARCHAR(30),
+	level INT,
+	exp INT,
+	hp INT,
+	strength INT,
+	intelligence INT,
+	dexterity INT,
+	wisdom INT
+)
+''' 
+
+cursor.execute(create_character_table_query)
+conn.commit()
+
+
+
+################## Insert Character Data in PostGRES ###################
+# INSERT DATA
+
+# for character in characters:
+
+#     insert_query = f'''INSERT INTO rpg_characters 
+#     (character_id, name, level, exp, hp, strength, intelligence, dexterity, wisdom)
+#     VALUES {character}
+#     '''
+#     # test it first in print()
+#     # print(insert_query)
+#     #then use cursor
+#     cursor.execute(insert_query)
+
+
+
+### FROM DS 15 timestamp 1:51:13
+
+my_dict = {"a":1, "b": ["dog", "cat", 42], "c": 'true'}
+# insertion_query = "INSERT INTO test_table (name, data) VALUES (%s, %s)"
+## This is object oriented approach with cursor.
+# cursor.execute(insertion_query,
+#     ('A rowwwwww', 'null'))
+
+# cursor.execute(insertion_query,
+#     ('Another row with a JSONNNN', json.dumps(my_dict))
+# )
+
+
+insertion_query = "INSERT INTO test_table (name, data) VALUES %s"
+## This is functional approach using execute_values
+execute_values(cursor, insertion_query, [
+    ('A rowwww', 'null'),
+    ('Another row, with JSOOONNNNN', json.dumps(my_dict)),
+    ('Third row', '3')
+]) # data must be in a list of tuples!!!!!
+
+
+
+
+# ACTUALLY SAVES TRANSACTIONS
+conn.commit() # commits to DB
